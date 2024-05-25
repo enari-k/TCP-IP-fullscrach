52079408a9d1538758a7f83e3b0f76a2a0f4dd22
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 05:31:47 +0900

    Completion of step02

--- net.c
+++ net.c
@@ -85,7 +85,13 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
     return 0;
 }
 
-int net_init(void)
+int
+net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
+{
+}
+
+int
+net_init(void)
 {
     infof("initializa...");
     if (platform_init() == -1)
